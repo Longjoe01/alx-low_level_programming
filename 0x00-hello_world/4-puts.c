3f4975a307615c07f58fa208ef3643ee4f282be6
Joseph <josbayo09@gmail.com>
2022-10-13 18:52:38 -0700

    Using put to print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Printing
+ *
+ * Return: Always 0 (Succesful)
+ */
+
+int main(void)
+{
+	char *s = "Programming is like building a multilingual puzzle";
+
+	puts(s);
+	return (0);
+
+}
