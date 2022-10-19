200e7a7047ef46736d9829f3cae27ee40402f1c1
Joseph <josbayo09@gmail.com>
2022-10-19 07:38:28 -0700

    check for lower and upper alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabet
+ *
+ * @c: is a ASCII character
+ *
+ * Return: 1 if c is an alphabet, lower or upper
+ */
+int _isalpha(int c)
+{
+	for ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
