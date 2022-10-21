a1261cedd631b608578a71a3df528a9b0ca5452f
Joseph <josbayo09@gmail.com>
2022-10-21 15:12:57 -0700

    print numbers except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_most_numbers - prints number from 0 to 9
+* except 2 and 4
+*
+*Return: 0
+*/
+
+void print_most_numbers(void)
+{
+	for ((n = 0; n < 10; n++) && (n != 2 && n != 4))
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
