9edae91c1d43d7dd9654c5d5308f4885abab4f22
Joseph <josbayo09@gmail.com>
2022-10-21 15:27:50 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,14 @@
 
 void print_most_numbers(void)
 {
-	for ((n = '0'; n <= '9'; n++) && (n != '2'; n != '4'))
+	int n = '0';
+
+	for ((n <= '9'; n++)
 	{
+		if (n != '2' && n != '4')
+		{
 		_putchar(n);
+		}
 	}
 	_putchar('\n');
 }
