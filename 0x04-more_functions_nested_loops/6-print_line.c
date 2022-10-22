f520cf41bc690a7f41b836e98bbe21ac6123e388
Joseph <josbayo09@gmail.com>
2022-10-21 18:23:22 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_line - a function that prints line in the terminal
+* follow by a newline
+* @n: an integer
+*Return: 0
+*/
+void print_line(int n)
+{
+	int l = 0;
+
+	if (n > 0)
+	{
+	for (; i < n; i++)
+	_putchar('_');
+	}
+	_putchar('\n');
+}
