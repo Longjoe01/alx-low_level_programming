61a48859562366947e585435a19c3e0fd460ef0f
Joseph <josbayo09@gmail.com>
2022-10-23 09:50:23 -0700

    fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,9 +15,9 @@ int main(void)
 		else if (i % 3 == 0)
 		printf("Fizz ");
 		else if (i % 5 == 0)
-		printf("Buzz");
+		printf("Buzz ");
 		else
-		printf("%d\n");
+		printf("%d\n" i);
 	}
 	return (0);
 }
