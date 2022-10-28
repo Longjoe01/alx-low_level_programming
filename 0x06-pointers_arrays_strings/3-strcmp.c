13a6660da9d1e85df0b0496ab32de9fa99837ae9
Joseph <josbayo09@gmail.com>
2022-10-28 11:07:45 -0700

    hot fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,17 +6,17 @@
 * @s2: second input parameter
 * Return: value
 */
+
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int value = 0;
+	int r;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	i = 0;
+	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0')
 	{
-		if (s1[i] != s2[i])
-		{
-			value = 1;
-			break;
-		}
+	i++;
 	}
+	r = s1[i] - s2[i];
+	return (r);
 }
