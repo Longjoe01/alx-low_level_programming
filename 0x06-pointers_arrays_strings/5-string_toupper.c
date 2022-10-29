d91516d86c1511610b6419f3ba8cb2c74aae7d2b
Joseph <josbayo09@gmail.com>
2022-10-29 12:13:50 -0700

    hot fixed

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,10 +13,8 @@ char *string_toupper(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= 97; &&s[i] <= 122)
-		{
-			s[i] = s[i] - 32;
-		}
+		if (s[i] >= 'a'; &&s[i] <= 'z')
+		s[i] = s[i] - 32;
 	}
 	return (s);
 }
