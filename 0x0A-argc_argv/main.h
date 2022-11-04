a924bacabe4f41751f5e900eeb540c0e9bb41341
Joseph <josbayo09@gmail.com>
2022-11-04 12:23:26 -0700

    clear

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+
+
+int _putchar(char c);
+__attribute__((unused));
+(void);
+int main(int argc __attribute__((unused)), char *argv[]) 
+
+
+
+
+#endif /* MAIN_H */
