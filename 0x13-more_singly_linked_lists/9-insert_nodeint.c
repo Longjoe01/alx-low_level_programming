b8382de9b9fb3863ff858aa7773edd140750427c
Joseph <josbayo09@gmail.com>
2022-11-30 08:07:24 -0800

    updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	ptr2->n = n;
 	ptr2->next = NULL;
-	while (pos != (idx))
+	while (pos != (idx - 1))
 	{
 		ptr = ptr->next;
 		pos++;
